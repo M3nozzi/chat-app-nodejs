529f9e15c8ea688a2812163a3b5a77abdd514260
Fabio Menozzi <f.menozzi@yahoo.com>
2020-04-24 10:26:15 -0300

    done

---
+++ chatApp.js
@@ -0,0 +1,25 @@
+const chatApp = server => {
+    // server side socket instantiation
+    const io = require('socket.io')(server);
+  
+    io.on('connection', socket => {
+
+        const userId = socket.conn.id.slice()
+        
+        console.log('socket connected, id: ', userId);
+        
+      io.emit('chatMessages', 'new user connected. total: ' + socket.conn.server.clientsCount);
+  
+        socket.on('message', message => {
+          
+        socket.broadcast.emit('chatMessages', userId + ' : ' + message);
+      });
+  
+      socket.on('disconnect', () => {
+        io.emit('chatMessages', 'user disconnected. total: ' + socket.conn.server.clientsCount);
+        console.log('disconnected');
+      });
+    });
+  }
+  
+  module.exports = chatApp;
